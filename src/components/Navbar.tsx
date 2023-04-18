4623b4854695e34fb9a05e6aa5ed7b19a9c245f5
majapiszczek <piszczekmaja33@gmail.com>
2023-04-18 11:29:00 +0200

    added login auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import { sign } from "crypto";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
+      <div>
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img src={user?.photoURL || ""} />
+            <button onClick={signUserOut}>Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
